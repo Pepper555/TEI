109182e75c519bdfb1dc208940981a1a4d995ac7
Pedro Henrique Diniz <95726751+Pepper555@users.noreply.github.com>
2022-07-08 08:42:45 -0300

    Update fon_fon.ino

--- fon_fon.ino
+++ fon_fon.ino
@@ -1,8 +1,8 @@
 #define fon 6
 #define luz 3
-#define pisca 5
-#define botao 10
-
+#define pisca 4
+#define botao 2
+volatile bool estado = false;
 //1. Projete um alarme usando um buzzer, que inicie com frequencia de 1500hz e decremente a frequencia em 100hz a cada delay 100 até a frequencia atingir 0
 
  //2. Acione um led ligado constantemente. Ao apertar um botão usado como interrupção outro led acenderá e piscará por um delay 500.
@@ -11,31 +11,30 @@ void setup() {
   pinMode(luz, OUTPUT);
   pinMode(pisca, OUTPUT);
   pinMode(botao, INPUT);
+  attachInterrupt(digitalPinToInterrupt(botao), pisca_pisca, CHANGE);
 }
 
 void loop() {
   digitalWrite(luz, HIGH);
-  if(botao == HIGH){
+}
+void pisca_pisca(){
+    if(estado){
     digitalWrite(luz, LOW);
-    delay(100);
     while(true){
-      if(botao == HIGH){
-        break;
-      }
-      digitalWrite(pisca, HIGH);
-      delay(500);
-      digitalWrite(pisca, LOW);
+      
+      int i=1500;
+      while(i > 0){
+        tone(fon, i);
+        digitalWrite(pisca, HIGH);
+        noTone(fon);
+        digitalWrite(pisca, LOW);
+        i=i-100;
+        }
     }
+    estado = false;
   }
-
-int i=1500;
-  while(i >= 0){
-    tone(fon, i);
-    
-    delay(100);
-    noTone(fon);
-    delay(100);
-    i=i-100;
+  else{
+    digitalWrite(luz, HIGH);
+    estado = true;
   }
-  
 }
